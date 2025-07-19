6b258234ef91156aedaca8472010a444193ef072
Roman <papa.apy@gmail.comgit config --global user.email papa.apy@gmail.com>
2025-07-19 15:25:15 +0200

    Add project files.

---
+++ VegetableShop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VegetableShopApp
+{
+    public class VegetableShop
+    {
+        private List<Vegetable> products = new List<Vegetable>();
+
+        public void AddProduct(Vegetable product)
+        {
+            products.Add(product);
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine("\n=== Список продуктів ===");
+            decimal totalPrice = 0;
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.GetInfo());
+                totalPrice += product.Price;
+            }
+            Console.WriteLine($"Загальна ціна всіх продуктів: {totalPrice:C}");
+        }
+    }
+}
